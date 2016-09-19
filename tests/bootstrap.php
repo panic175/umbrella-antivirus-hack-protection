6df746779baa5a23c2f7720c1e063b9f2a944e19
Rasmus Kjellberg <rk@youngmedia.se>
2016-09-19 23:48:04 +0200

    Test plugin with Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,11 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+define('PLUGIN_NAME','init.php');
+define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
+define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
+define('PLUGIN_ABSOLUTE_PATH', dirname( dirname( __FILE__ ) ) . '/init.php' );
+
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
@@ -17,7 +22,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/init.php';
+	require PLUGIN_ABSOLUTE_PATH;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
---
+++ tests/test-plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class PluginTest
+ *
+ * @package Umbrella_Antivirus_Hack_Protection
+ */
+
+/**
+ * Sample test case.
+ */
+class PluginTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+
+  // Check that plugin versions in init.php matches
+  function test_plugin_version_tag() {
+    $plugin_data = get_plugin_data( PLUGIN_ABSOLUTE_PATH, false, false );
+    $this->assertTrue( UMBRELLA__VERSION==$plugin_data['Version'] );
+  }
+}
